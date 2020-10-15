6385bbde85418e1a919b91942e2b385ff75f3c85
lamiasristy <lamiasristy@gmail.com>
2020-10-15 12:14:01 +0200

    Added the model Comment with validation and association.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
     validates :title, presence: true, uniqueness: true
     validates :body, presence: true
+    
     belongs_to :user
+    has_many :comments
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { in: 6..16 }, presence: true
+  
   has_many :posts
+  has_many :comments
 end
