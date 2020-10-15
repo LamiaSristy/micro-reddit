9d97259d11f0e8eda98d70cc2032fef54689c832
lamiasristy <lamiasristy@gmail.com>
2020-10-15 10:53:03 +0200

    User model created with validation

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 6..16 }, presence: true
+end
